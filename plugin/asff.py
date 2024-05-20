a5715f55c4414a7b798cc7f9df8f1b3ffc3844b7
Damien De Campos <damiensporting@gmail.com>
2024-05-20 22:37:19 +0200

    updating plugin

--- plugin/asff.py
+++ plugin/asff.py
@@ -2,10 +2,10 @@ from GPS import Locations, Menu, File, MDI, Process, Project, Preference
 from gs_utils import interactive
 import os.path
 
-Preference_Name = "Plugins/AdaSearch/Limit percentage"
+Preference_Name = "Plugins/Asff/Limit percentage"
 Preference(Preference_Name).create("Display matches with scores above the given percentage of the top score", "integer", "20")
 
-class Ada_Search:
+class Asff:
 
     def __init__(self, query):
         self.query = query
@@ -17,7 +17,7 @@ class Ada_Search:
         col = parts[2]
         name = parts[3][1:]
         query = self.query
-        Locations.add(category="Ada Search: " + query,
+        Locations.add(category="asff: " + query,
                           file=File(file_path),
                           line=int(line),
                           column=int(col),
@@ -25,15 +25,15 @@ class Ada_Search:
                           highlight="Search highlighting",
                           length = len(name))
 
-    def search(self):
+    def find(self):
         project = os.path.basename(Project.root().file().path)
         regex = "^.+:[0-9]+:[0-9]+:.*$"
-        Process (["ada_search", self.query, "-P" + project, "--name-only", "--limit-percentage", str(Preference(Preference_Name).get())], regex, on_match=self.add_location)
+        Process (["asff", "-q", self.query, "-P" + project, "--name-only", "--limit-percentage", str(Preference(Preference_Name).get())], regex, on_match=self.add_location)
 
 
 @interactive(name='open test console',
-             menu='/Find/Find subprogram')
-def open_ada_search_console():
-    query = MDI.input_dialog("Search Query", "Query")[0]
-    s = Ada_Search(query)
-    s.search()
+             menu='/Find/Fuzzy find subprogram')
+def open_asff_console():
+    query = MDI.input_dialog("Ada Subprogram fuzzy finder", "Query")[0]
+    s = Asff(query)
+    s.find()
